0303c6285c0b5d7c04ca4d89ea77f7f9aca0ece1
Flávio Alcântara <fcsalcantara@gmail.com>
2017-05-05 02:30:43 -0300

    Commit inicial

---
+++ src/br/com/financas/model/Conta.java
@@ -0,0 +1,44 @@
+package br.com.financas.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Conta {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String titular;
+	private String banco;
+	private String agencia;
+	private String numero;	
+	
+	public Conta(String titular, String banco, String agencia, String numero) {
+		this.titular = titular;
+		this.banco = banco;
+		this.agencia = agencia;
+		this.numero = numero;
+	}
+	
+//	public Conta() { }
+	
+	public String getTitular() { return titular; }
+	
+	public String getBanco() { return banco; }
+	
+	public String getAgencia() { return agencia; }
+	
+	public String getNumero() { return numero; }
+	
+//	public void setTitular(String titular) { this.titular = titular; }
+//	
+//	public void setBanco(String banco) { this.banco = banco; }
+//	
+//	public void setAgencia(String agencia) { this.agencia = agencia; }
+//	
+//	public void setNumero(String numero) { this.numero = numero; }
+	
+}
